cbe3fe0ed9f1f6a4757ae9d0538cc0e1206ca980
ngmh <ngmhcartoon@gmail.com>
2025-05-27 18:16:50 +0800

    Move database interactions into another file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import env from "dotenv";
 import multer from "multer";
 
-import { createClient } from "@supabase/supabase-js";
+import * as database from "./database.js";
 
 env.config();
 
@@ -10,34 +10,14 @@ const app = express();
 const upload = multer();
 const port = 3000;
 
-const supabase = createClient(process.env.DATABASE_URL, process.env.DATABASE_KEY);
-
 app.get("/", (req, res) => {
 	res.send("Hello Lol!");
 	return;
 });
 
-async function signUpNewUser(email, password) {
-	const { data, error } = await supabase.auth.signUp({
-		email: email,
-		password: password,
-	});
-	console.log({ data, error });
-	return { data, error };
-}
-
-async function signInWithEmail(email, password) {
-	const { data, error } = await supabase.auth.signInWithPassword({
-		email: email,
-		password: password,
-	});
-	console.log({ data, error });
-	return { data, error };
-}
-
 app.post("/signup", upload.none(), async (req, res) => {
 	console.log("Signing up", req.body.email, req.body.password);
-	const { data, error } = await signUpNewUser(req.body.email, req.body.password);
+	const { data, error } = await database.signUpNewUser(req.body.email, req.body.password);
 	if (error == null) {
 		res.send(data);
 		return;
@@ -60,7 +40,7 @@ app.post("/signup", upload.none(), async (req, res) => {
 
 app.post("/login", upload.none(), async (req, res) => {
 	console.log("Signing in", req.body.email, req.body.password);
-	const { data, error } = await signInWithEmail(req.body.email, req.body.password);
+	const { data, error } = await database.signInWithEmail(req.body.email, req.body.password);
 	if (error == null) {
 		res.send(data);
 		return;
@@ -73,29 +53,8 @@ app.post("/login", upload.none(), async (req, res) => {
 	return;
 });
 
-async function getUser(access_token) {
-	const { data, error } = await supabase.auth.getUser(access_token);
-	return { data, error };
-}
-
-async function updateTest(access_token, username) {
-	const { data: userData, error: userError } = await getUser(access_token);
-	if (userError != null) {
-		return { data: userData, error: userError };
-	}
-	const user = userData.user;
-	const { data, error } = await supabase
-		.from("test")
-		.upsert({
-			user: user.id,
-			username: username,
-		}, { onConflict: "user" })
-	return { data, error };
-}
-
-
 app.post("/test", upload.none(), async (req, res) => {
-	const { data, error } = await updateTest(req.body.access_token, req.body.username);
+	const { data, error } = await database.updateTest(req.body.access_token, req.body.username);
 	if (error == null) {
 		res.send("Success");
 		return;
